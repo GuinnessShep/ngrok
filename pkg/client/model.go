d6477b35bd54c087def8aca3af4d3a050cc9fdaf
Raphael Stefanini <rphl@rphl.net>
2021-06-22 21:38:00 +0200

    Edit default server address

--- pkg/client/model.go
+++ pkg/client/model.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	defaultServerAddr   = "ngrokd.ngrok.com:443"
+	defaultServerAddr   = "io.rphlo.com:4443"
 	defaultInspectAddr  = "127.0.0.1:4040"
 	pingInterval        = 20 * time.Second
 	maxPongLatency      = 15 * time.Second
